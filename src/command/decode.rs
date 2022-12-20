ee3c3885d29dde782a2ea718990a8d3bbc65cb3d
Jacob <encody@users.noreply.github.com>
2022-12-20 18:56:23 +0900

    chore: decode tests

--- src/command/decode.rs
+++ src/command/decode.rs
@@ -17,12 +17,12 @@ pub struct DecodeArgs {
     pub output_path: Option<PathBuf>,
 }
 
-pub struct Decode {
-    pub input: serde_json::Value,
-    pub output: Box<dyn Write>,
+pub struct Decode<'a> {
+    pub input: Vec<u8>,
+    pub output: Box<dyn Write + 'a>,
 }
 
-impl TryFrom<&'_ DecodeArgs> for Decode {
+impl TryFrom<&'_ DecodeArgs> for Decode<'_> {
     type Error = IOError;
 
     fn try_from(
@@ -32,25 +32,79 @@ impl TryFrom<&'_ DecodeArgs> for Decode {
         }: &'_ DecodeArgs,
     ) -> Result<Self, Self::Error> {
         Ok(Self {
-            input: {
-                let input_bytes = get_input_bytes(input_path.as_ref())?;
-
-                let mut buf = &input_bytes as &[u8];
-
-                let schema = <BorshSchemaContainer as BorshDeserialize>::deserialize(&mut buf)
-                    .map_err(|_| IOError::DeserializeBorsh("schema"))?;
-
-                let value = crate::dynamic_schema::deserialize_from_schema(&mut buf, &schema)
-                    .map_err(|_| IOError::DeserializeBorsh("data according to embedded schema"))?;
-                value
-            },
+            input: get_input_bytes(input_path.as_ref())?,
             output: output_writer(output_path.as_ref())?,
         })
     }
 }
 
-impl Execute for Decode {
+impl Execute for Decode<'_> {
     fn execute(&mut self) -> Result<(), IOError> {
-        output_json(&mut self.output, &self.input)
+        let mut buf = &self.input as &[u8];
+
+        let schema = <BorshSchemaContainer as BorshDeserialize>::deserialize(&mut buf)
+            .map_err(|_| IOError::DeserializeBorsh("schema"))?;
+
+        let value = crate::dynamic_schema::deserialize_from_schema(&mut buf, &schema)
+            .map_err(|_| IOError::DeserializeBorsh("data according to embedded schema"))?;
+
+        output_json(&mut self.output, &value)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io::BufWriter;
+
+    use borsh::{BorshDeserialize, BorshSchema, BorshSerialize};
+    use serde::{Deserialize, Serialize};
+
+    use crate::command::Execute;
+
+    use super::Decode;
+
+    #[derive(Serialize, Deserialize, BorshSerialize, BorshDeserialize, BorshSchema, Debug)]
+    struct Parent {
+        integer: u32,
+        vector: [u8; 8],
+        child: Child,
+    }
+
+    #[derive(
+        Serialize, Deserialize, BorshSerialize, BorshDeserialize, BorshSchema, PartialEq, Debug,
+    )]
+    struct Child {
+        string: String,
+        boolean: bool,
+    }
+
+    #[test]
+    fn with_schema() {
+        let value = Parent {
+            integer: 24,
+            vector: [8, 7, 6, 5, 4, 3, 2, 1],
+            child: Child {
+                string: "()".to_string(),
+                boolean: false,
+            },
+        };
+
+        let mut output_vector: Vec<u8> = vec![];
+        let writer = BufWriter::new(&mut output_vector);
+
+        let mut p = Decode {
+            input: borsh::try_to_vec_with_schema(&value).unwrap(),
+            output: Box::new(writer),
+        };
+
+        p.execute().unwrap();
+        drop(p);
+
+        let expected = serde_json::to_value(&value).unwrap();
+
+        assert_eq!(
+            expected,
+            serde_json::from_slice::<serde_json::Value>(&output_vector).unwrap()
+        );
     }
 }
